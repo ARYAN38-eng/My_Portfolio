0a022407e1cbfd3d9331c46fa0861854fb77fa1e
ARYAN38-eng <aryanraina2021@gmail.com>
2025-07-18 16:20:42 +0530

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import ScrollToTop from "./components/scrollToTop";
-import CustomCursor from "./components/customCursor";
+import CustomCursor from "./components/CustomCursor";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Qualifications from "./pages/Qualifications";
