3c99a7773c50a920fff638c1534419589d4f4a00
ARYAN38-eng <aryanraina2021@gmail.com>
2025-07-18 16:15:56 +0530

    updated CustomCursor to customCursor

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -1,5 +1,5 @@
 // components/CustomCursor.jsx
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 const CustomCursor = () => {
